f62f40529b979971c272da182400e303315ebfc8
Kactus83 <93870523+Kactus83@users.noreply.github.com>
2023-03-17 18:15:17 +0100

    WIP UI upgrade

--- gui/main-gui.js
+++ gui/main-gui.js
@@ -1,36 +1,89 @@
-function createAddPlayerForm() {
-  const form = document.createElement('form');
-  form.id = 'add-player-form';
+function createPlayerSelect(localDatas) {
+  const select = document.createElement('select');
+  select.id = 'playerSelect';
 
-  const input = document.createElement('input');
-  input.type = 'text';
-  input.name = 'name';
-  input.placeholder = 'Nom du joueur';
-  form.appendChild(input);
-
-  const button = document.createElement('button');
-  button.type = 'submit';
-  button.textContent = 'Ajouter';
-  form.appendChild(button);
-
-  return form;
+  if (localDatas.players.length === 0) {
+    const option = document.createElement('option');
+    option.value = '';
+    option.text = 'Aucun joueur enregistré';
+    select.add(option);
+  } else {
+    localDatas.players.forEach(player => {
+      const option = document.createElement('option');
+      option.value = player.name;
+      option.text = player.name;
+      select.add(option);
+    });
+  }
+  return select;
 }
 
-function replaceNickHolderWithForm() {
+function togglePlayerForm() {
+  const form = document.getElementById('playerForm');
+  form.style.display = form.style.display === 'none' ? 'block' : 'none';
+}
+
+async function replaceNickHolderWithForm() {
+  const [
+    { registerPlayer },
+    { loadLocalDatas }
+  ] = await Promise.all([
+    import('../utils/player-service.js'),
+    import('../utils/local-datas.js')
+  ]);
+
+  const localDatas = await loadLocalDatas();
+  const playerSelect = createPlayerSelect(localDatas);
   const nickHolder = document.getElementById('nick_holder');
+  const parentElement = nickHolder.parentElement;
 
-  if (!nickHolder) {
-    setTimeout(replaceNickHolderWithForm, 1000);
-    return;
-  }
+  // Remplacer nick_holder par playerSelect
+  parentElement.insertBefore(playerSelect, nickHolder);
+  parentElement.removeChild(nickHolder);
 
-  const addPlayerForm = createAddPlayerForm();
-  nickHolder.replaceWith(addPlayerForm);
+  // Ajouter un bouton pour afficher/masquer le formulaire d'ajout de joueur
+  const toggleFormButton = document.createElement('button');
+  toggleFormButton.textContent = 'Ajouter un joueur';
+  toggleFormButton.onclick = togglePlayerForm;
+  parentElement.appendChild(toggleFormButton);
 
-  addPlayerForm.addEventListener('submit', (event) => {
+  // Créer le formulaire d'ajout de joueur et le masquer si des joueurs sont enregistrés
+  const playerForm = document.createElement('form');
+  playerForm.id = 'playerForm';
+  playerForm.style.display = localDatas.players.length === 0 ? 'block' : 'none';
+
+  const playerNameInput = document.createElement('input');
+  playerNameInput.type = 'text';
+  playerNameInput.placeholder = 'Nom du joueur';
+  playerForm.appendChild(playerNameInput);
+
+  const submitButton = document.createElement('button');
+  submitButton.type = 'submit';
+  submitButton.textContent = 'Enregistrer';
+  playerForm.appendChild(submitButton);
+
+  playerForm.onsubmit = (event) => {
     event.preventDefault();
-    console.log('Le formulaire a été soumis !');
-  });
+    const playerName = playerNameInput.value.trim();
+    if (playerName !== '') {
+      if (registerPlayer(playerName)) {
+        const option = document.createElement('option');
+        option.value = playerName;
+        option.text = playerName;
+        playerSelect.add(option);
+        playerSelect.value = playerName;
+        playerNameInput.value = '';
+        if (localDatas.players.length === 1) {
+          // Masquer le formulaire s'il y avait auparavant 0 joueur enregistré
+          togglePlayerForm();
+        }
+      } else {
+        alert('Ce nom de joueur existe déjà.');
+      }
+    }
+  };
+
+  parentElement.appendChild(playerForm);
 }
 
 replaceNickHolderWithForm();
--- utils/local-datas.js
+++ utils/local-datas.js
@@ -1,13 +1,15 @@
 import { LocalDatas } from '../models.js';
 
 export function loadLocalDatas() {
-    chrome.storage.local.get('localDatas', function(data) {
-        let localDatas = data.localDatas;
-        if (!localDatas) {
-            localDatas = new LocalDatas();
-            chrome.storage.local.set({localDatas: localDatas});
-        }
-        return localDatas;
+    return new Promise((resolve) => {
+        chrome.storage.local.get('localDatas', function(data) {
+            let localDatas = data.localDatas;
+            if (!localDatas) {
+                localDatas = new LocalDatas();
+                chrome.storage.local.set({localDatas: localDatas});
+            }
+            resolve(localDatas);
+        });
     });
 }
 
