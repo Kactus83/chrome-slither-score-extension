1e8cf01ef9bb82e6e336202e1d6cab0da610e1fc
Kactus83 <93870523+Kactus83@users.noreply.github.com>
2023-03-21 09:28:04 +0100

    WIP Handling Sessions & Scores
    - update messages.js in content

--- content/messages/messages.js
+++ content/messages/messages.js
@@ -28,3 +28,19 @@ export function sendGetAllData() {
 export function sendRegisterPlayer(playerName) {
   return sendRequest({ type: 'REGISTER_PLAYER', playerName });
 }
+
+export function sendStartSession(playerNames) {
+  return sendRequest({ type: 'START_SESSION', playerNames });
+}
+
+export function sendEndSession() {
+  return sendRequest({ type: 'END_SESSION' });
+}
+
+export function sendUpdateSession(updatedSession) {
+  return sendRequest({ type: 'UPDATE_SESSION', updatedSession });
+}
+
+export function sendAddSessionScore(playerName, value, date) {
+  return sendRequest({ type: 'ADD_SESSION_SCORE', playerName, value, date });
+}
