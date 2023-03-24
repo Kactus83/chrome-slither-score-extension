60834e68685cc4f827bbeb5158208a054d19316c
Kactus83 <93870523+Kactus83@users.noreply.github.com>
2023-03-24 08:46:22 +0100

    REFACTOR messages in content :
    - creating modular architecture for cleaner code

--- content/messages/send-messages.js
+++ content/messages/send-messages.js
@@ -1,4 +1,8 @@
 import { onBackgroundMessage } from "./handle-messages.js";
+import { sendGetPlayerNameAvailability, sendGetPlayerBestScore, sendGetSessionAverageScore, sendGetNextPlayer, sendGetBestScoreRanking, sendGetAverageScoreRanking, sendGetPlayerScoreCount, sendGetPlayerTotalScore, sendGetTotalScores, sendGetLastScore } from './modules/get-datas';
+import { sendPageVisited, sendPageLoaded, sendLaunchAddPlayerToDatas, sendLaunchInitGameSession } from './modules/page-and-routing';
+import { sendInitGameSession, sendResumeGameSession, sendEndGameSession, sendSelectedPlayer, sendAddPlayerToDatas } from './modules/user-actions';
+import { sendGameStatusWaitTurn, sendGameStatusInProgress, sendGameStatusGameOver } from './modules/game-states';
 
 async function sendRequest(request) {
   console.log("sending request : ");
@@ -12,99 +16,27 @@ async function sendRequest(request) {
   });
 }
 
-// PAGE EVENTS & ROUTING REQUEST
-
-export function sendPageVisited() {
-  return sendRequest({ type: 'PAGE_VISITED' });
-}
-
-export function sendPageLoaded() {
-  return sendRequest({ type: 'PAGE_LOADED' });
-}
-
-export function sendLaunchAddPlayerToDatas(playerName) {
-  return sendRequest({ type: 'LAUNCH_ADD_PLAYER_TO_DATAS', playerName });
-}
-
-export function sendLaunchInitGameSession() {
-  return sendRequest({ type: 'LAUNCH_INIT_GAME_SESSION' });
-}
-
-// USER ACTION REQUEST
-
-export function sendInitGameSession(playersNames) {
-  return sendRequest({ type: 'INIT_GAME_SESSION', playersNames });
-}
-
-export function sendResumeGameSession() {
-  return sendRequest({ type: 'RESUME_GAME_SESSION' });
-}
-
-export function sendEndGameSession() {
-  return sendRequest({ type: 'END_GAME_SESSION' });
-}
-
-export function sendSelectedPlayer(playerName) {
-  return sendRequest({ type: 'SELECT_ACTIVE_PLAYER', playerName });
-}
-
-export function sendAddPlayerToDatas(playerName) {
-  return sendRequest({ type: 'ADD_PLAYER_TO_DATAS', playerName });
-}
-
-// GAME SATES REQUESTS
-
-export function sendGameStatusWaitTurn() {
-  return sendRequest({ type: 'WAIT_NEXT_TURN' });
-}
-
-export function sendGameStatusInProgress(playerName) {
-  return sendRequest({ type: 'IN_PROGRESS', playerName });
-}
-
-export function sendGameStatusGameOver(playerScore) {
-  return sendRequest({ type: 'GAME_OVER', playerScore });
-}
-
-// GET DATAS request 
-
-export function sendGetPlayerNameAvailability(playerName) {
-  return sendRequest({ type: 'GET_PLAYER_NAME_AVAILABILITY', playerName });
-}
-
-export function sendGetPlayerBestScore(playerName) {
-  return sendRequest({ type: 'GET_PLAYER_BEST_SCORE', playerName });
-}
-
-export function sendGetSessionAverageScore() {
-  return sendRequest({ type: 'GET_SESSION_AVERAGE_SCORE' });
-}
-
-export function sendGetNextPlayer() {
-  return sendRequest({ type: 'GET_NEXT_PLAYER' });
-}
-
-export function sendGetBestScoreRanking() {
-  return sendRequest({ type: 'GET_BEST_SCORE_RANKING' });
-}
-
-export function sendGetAverageScoreRanking() {
-  return sendRequest({ type: 'GET_AVERAGE_SCORE_RANKING' });
-}
-
-export function sendGetPlayerScoreCount(playerName) {
-  return sendRequest({ type: 'GET_PLAYER_SCORE_COUNT', playerName });
-}
-
-export function sendGetPlayerTotalScore(playerName) {
-  return sendRequest({ type: 'GET_PLAYER_TOTAL_SCORE', playerName });
-}
-
-export function sendGetTotalScores() {
-  return sendRequest({ type: 'GET_TOTAL_SCORES' });
-}
-
-export function sendGetLastScore() {
-  return sendRequest({ type: 'GET_LAST_SCORE' });
-}
-
+export {
+  sendPageVisited,
+  sendPageLoaded,
+  sendLaunchAddPlayerToDatas,
+  sendLaunchInitGameSession,
+  sendInitGameSession,
+  sendResumeGameSession,
+  sendEndGameSession,
+  sendSelectedPlayer,
+  sendAddPlayerToDatas,
+  sendGameStatusWaitTurn,
+  sendGameStatusInProgress,
+  sendGameStatusGameOver,
+  sendGetPlayerNameAvailability,
+  sendGetPlayerBestScore,
+  sendGetSessionAverageScore,
+  sendGetNextPlayer,
+  sendGetBestScoreRanking,
+  sendGetAverageScoreRanking,
+  sendGetPlayerScoreCount,
+  sendGetPlayerTotalScore,
+  sendGetTotalScores,
+  sendGetLastScore
+};
