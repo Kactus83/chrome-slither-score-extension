edf34fb5dec8327f36bc0710f82ed5e89dad795a
Kactus83 <93870523+Kactus83@users.noreply.github.com>
2023-03-29 12:20:26 +0200

    Debuging Ok

--- content/gui/modules/new-turn/utils/login-watcher.js
+++ content/gui/modules/new-turn/utils/login-watcher.js
@@ -9,22 +9,25 @@ export class LoginDivObserver {
 
   observe() {
     if (!this.loginDiv) return;
-
+  
     this.observer = new MutationObserver(mutations => {
       mutations.forEach(mutation => {
-        if (!this.inProgressTriggered && mutation.type === 'attributes' && mutation.attributeName === 'style' && this.loginDiv.style.display === 'none') {
+        const bskhDiv = document.querySelector('#bskh');
+        const bskhDivVisible = bskhDiv && bskhDiv.style.display !== 'none';
+  
+        if (!this.inProgressTriggered && mutation.type === 'attributes' && mutation.attributeName === 'style' && this.loginDiv.style.display === 'none' && !bskhDivVisible) {
           this.inProgressTriggered = true;
           this.stop();
           this.triggerInitInProgress();
         }
       });
     });
-
+  
     const config = { attributes: true };
     this.observer.observe(this.loginDiv, config);
-
+  
     return this.observer;
-  }
+  }  
 
   stop() {
     if (this.observer) {
